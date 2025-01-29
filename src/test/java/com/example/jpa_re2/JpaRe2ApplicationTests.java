486fa1196a7a6838ea04d210ae7c32a72af377a4
shinwoos <kmnj2100@gmail.com>
2025-01-30 02:48:06 +0900

    Initial commit

---
+++ src/test/java/com/example/jpa_re2/JpaRe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_re2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRe2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
